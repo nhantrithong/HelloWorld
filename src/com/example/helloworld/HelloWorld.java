0c45c0d53515576bd0381edb1368d576df23c198
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-03-22 11:29:01 +0800

    Chairs and table

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,9 +2,12 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Starboy - The Weeknd");
-        System.out.println("I'm OK - iKON");
-        System.out.println("Party Monster - The Weeknd");
-        System.out.println("CLC - No");
+        System.out.println("X                   X ");
+        System.out.println("X                   X ");
+        System.out.println("X      XXXXXXX      X ");
+        System.out.println("XXXXX  X     X  XXXXX ");
+        System.out.println("X   X  X     X  X   X ");
+        System.out.println("X   X  X     X  X   X ");
+
     }
 }
