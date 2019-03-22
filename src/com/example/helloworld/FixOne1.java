b28a6c7aecc5d99ab5d99f5d2d2c7a36b0243d26
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-03-22 11:42:44 +0800

    FixOne1 done

---
+++ src/com/example/helloworld/FixOne1.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class FixOne1 {
+    /* This program displays a greeting */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
