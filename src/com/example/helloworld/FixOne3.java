a7acc983bb87a95c6b646d3cc79278cc00fb7de4
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-03-22 11:48:58 +0800

    FixOne3 done

---
+++ src/com/example/helloworld/FixOne3.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class FixOne3 {
+    public static void main(String[] args) {
+        System.out.println("Over the river");
+        System.out.println("and through the woods");
+        System.out.println("to Grandmother's house we go");
+    }
+}
