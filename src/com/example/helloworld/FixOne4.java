538c5ad28432a12eabcef4b863ce199e03983b1e
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-03-22 11:52:00 +0800

    FixOne4 done

---
+++ src/com/example/helloworld/FixOne4.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+import javax.swing.JOptionPane;
+public class FixOne4 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "First GUI program");
+    }
+}
