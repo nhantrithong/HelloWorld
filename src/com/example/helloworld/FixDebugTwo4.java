27be4787d9a1966d7ff0b328b83b2e20af20c121
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-03-29 09:42:06 +0800

    FixDebugTwo4 done

---
+++ src/com/example/helloworld/FixDebugTwo4.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import javax.swing.JOptionPane;
+public class FixDebugTwo4
+{
+    public static void main(String[] args)
+    {
+        String costString;
+        double cost;
+        final double TAX = 0.06;
+        costString = JOptionPane.showInputDialog(null, "Enter price of item you are buying", "Price of Item", 
+                JOptionPane.INFORMATION_MESSAGE);
+        cost = Double.parseDouble(costString);
+        JOptionPane.showMessageDialog(null,"With " + TAX * 100 +
+                "% tax,  purchase  is $" + (cost - cost * TAX));
+    }
+}
