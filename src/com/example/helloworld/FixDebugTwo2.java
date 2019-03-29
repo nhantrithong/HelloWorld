d02bac03ee63819e61dfdbef4ad022cedc021504
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-03-29 09:19:15 +0800

    FixDebugTwo2 done

---
+++ src/com/example/helloworld/FixDebugTwo2.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+public class FixDebugTwo2
+//  This application performs arithmetic with two integers
+{
+    public static void main(String[] args)
+    {
+        int a, b;
+        a = 7;
+        b = 4;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        System.out.println("The sum is " + sum);
+        System.out.println("The difference is " + difference);
+        System.out.println("The product is " + product);
+    }
+}
