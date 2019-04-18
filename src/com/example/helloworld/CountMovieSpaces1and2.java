d7ff88ccaf3a33b6adaf6ad2d15502e1780fb9ce
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-04-18 10:26:15 +0800

    CountMovieSpaces1and2 done

---
+++ src/com/example/helloworld/CountMovieSpaces1and2.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class CountMovieSpaces1and2
+{
+    public static void main(String[] args)
+    {
+       Scanner scan = new Scanner(System.in);
+       System.out.println("Please enter a movie quote: ");
+       String quote = scan.nextLine();
+       quoteChecker(quote);
+    }
+
+    public static void quoteChecker(String quote)
+    {
+        int length = quote.replaceAll("[^ ]","").length();
+        System.out.println("The number of spaces in this quote is equal to: " + length + " spaces");
+    }
+
+}
+
+
