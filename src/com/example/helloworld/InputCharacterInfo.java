fb08397544c576c774c7479b4466b81b755e6157
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-04-20 17:17:30 +0800

    InputCharacterInfo done

---
+++ src/com/example/helloworld/InputCharacterInfo.java
@@ -0,0 +1,35 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class InputCharacterInfo
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a letter: ");
+        char aChar = scan.next().charAt(0);
+        System.out.println("The character is " + aChar);
+        if(Character.isUpperCase(aChar))
+            System.out.println(aChar + " is uppercase");
+        else
+            System.out.println(aChar + " is not uppercase");
+        if(Character.isLowerCase(aChar))
+            System.out.println(aChar + " is lowercase");
+        else
+            System.out.println(aChar + " is not lowercase");
+        aChar = Character.toLowerCase(aChar);
+        System.out.println("After toLowerCase(), aChar is " + aChar);
+        aChar = Character.toUpperCase(aChar);
+        System.out.println("After toUpperCase(), aChar is " + aChar);
+        if(Character.isLetterOrDigit(aChar))
+            System.out.println(aChar + " is a letter or digit");
+        else
+            System.out.println(aChar +
+                    " is neither a letter nor digit");
+        if(Character.isWhitespace(aChar))
+            System.out.println(aChar + " is whitespace");
+        else
+            System.out.println(aChar + " is not whitespace");
+    }
+}
